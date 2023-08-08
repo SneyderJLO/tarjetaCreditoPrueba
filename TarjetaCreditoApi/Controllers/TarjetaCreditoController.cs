0b9cbcdcb778e22c8b7a4251f87be9ca9ff05322
NeysserTL <tenemriq@outlook.com>
2023-08-08 16:06:12 -0500

    api

--- TarjetaCreditoApi/Controllers/TarjetaCreditoController.cs
+++ TarjetaCreditoApi/Controllers/TarjetaCreditoController.cs
@@ -3,15 +3,15 @@ using TarjetaCreditoApi.Model;
 
 namespace TarjetaCreditoApi.Controllers
 {
-    [ApiController]
-    [Route("api/tarjetas")]
-    public class TarjetaCreditoController
-    {
-        [HttpGet]
-        public async Task<ActionResult<List<MTarjeta>>> getTarjetas()
-        {
+    //[ApiController]
+    //[Route("api/tarjetas")]
+    //public class TarjetaCreditoController
+    //{
+    //    [HttpGet]
+    //    //public async Task<ActionResult<List<MTarjeta>>> getTarjetas()
+    //    //{
 
 
-        }
-    }
+    //    //}
+    //}
 }
